5c3e37acc5c2abbb18ae8163cdf8dc301e17ff21
disreal48 <disreal48@googlemail.com>
2024-03-28 17:00:09 +0100

    latest merge commit

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -28458,7 +28458,7 @@ $RefreshReg$(_c, "LoginView");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","@parcel/transformer-js/src/esmodule-helpers.js":"3AUTT","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"blCJu","react":"21dqq"}],"4OGiN":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","@parcel/transformer-js/src/esmodule-helpers.js":"3AUTT","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"blCJu"}],"4OGiN":[function(require,module,exports) {
 var $parcel$ReactRefreshHelpers$73d1 = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
 var prevRefreshReg = window.$RefreshReg$;
 var prevRefreshSig = window.$RefreshSig$;
@@ -28603,6 +28603,6 @@ $RefreshReg$(_c, "SignupView");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","@parcel/transformer-js/src/esmodule-helpers.js":"3AUTT","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"blCJu","react":"21dqq"}]},["lXJ1q","Ifxvu","d8Dch"], "d8Dch", "parcelRequireaec4")
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","@parcel/transformer-js/src/esmodule-helpers.js":"3AUTT","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"blCJu"}]},["lXJ1q","Ifxvu","d8Dch"], "d8Dch", "parcelRequireaec4")
 
 //# sourceMappingURL=index.b4b6dfad.js.map
