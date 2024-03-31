5e15f586879d63fb25bb70c0af02dbd6f3eba89a
disreal48 <disreal48@googlemail.com>
2024-03-31 16:38:56 +0200

    styled with bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.title}
-    </div>
+    <Card className="h-100" onClick={() => onMovieClick(movie)} variant="link">
+      <Card.Img className="h-100" variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.genre}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
